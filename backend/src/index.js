8c892b4dec8ffbc17c9422280d2555e4e19d3fb9
MD Hamza Khan <mdhamzakhanjsr2003@gmail.com>
2026-03-04 16:40:53 +0530

    cors update

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-frontend.vercel.app"
+      "https://fullstack-chat-app.vercel.app"
     ],
     credentials: true,
   })
